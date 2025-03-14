4d5ca11b5804ec93db375cc191ae83f24eb0da8b
kemsguy7 <mattidungafa@gmail.com>
2025-03-14 18:27:00 +0100

    updates: fixed tailwind config issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,71 +1,11 @@
-
 /** @type {import('tailwindcss').Config} */
-
 export default {
-  content: ["./src/**/*.{js,jsx,ts,jsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        roboto: ['Roboto', 'sans-serif'],
-      },
-      width: {
-      
-      },
-      height: {
-      
-      },
-      gap: {
-       
-      },
-      opacity: {
-        0: "0",
-      },
-      lineHeight: {
-       
-      },
-      backgroundColor: {
-      
-      },
-      borderRadius: {
-        largeButton: "1.5rem",
-        mediumButton: "1.25rem",
-        custom: "20px",
-        lg: "10px",
-        xl: "20px",
-      },
-      colors: {
-        vestpiBlack: "#191919",
-        black: "#000000",
-        vestpiWhite: "#FFFFFF",  
-      },
-      fontSize: {  
-      },
-      boxShadow: {
-        'custom': '7px 7px 4px 0px rgba(0, 0, 0, 0.25)',
-      },
-      backgroundImage: {
-        'button-gradient': 'linear-gradient(90deg, #9738EB 0%, #FF1ACD 100%)',
-      },
-      spacing: {
-        "general-horizontal-margin": "10rem",
-        "general-margin": "8rem",
-      },
-      letterSpacing: {
-        "0.05em": "0.05em",
-      },
-      margin: {
-        general: "10rem",
-      },
-      keyframes: {
-        
-      },
-      animation: {
-        
-      }
-    },
-  },
-  variants: {},
-  plugins: [
-   
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
-};
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
